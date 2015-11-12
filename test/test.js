dc827c468c6ac156c1e9a73268b778e0713af196
Rudy <rudy.devisscher@gmail.com>
2015-11-12 14:28:37 +0100

    Histoire 1

--- test/test.js
+++ test/test.js
@@ -3,4 +3,13 @@
 var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
+    var engine = new Engine();
+    var size = 6;
+
+    for (var i = 0; i < size - 1; i++) {
+        for (var j = 0; j < size - 1; j++) {
+            assertNotEquals(engine.getCase(i, j), engine.getCase(i+1, j));
+            assertNotEquals(engine.getCase(i, j), engine.getCase(i, j+1));
+        }
+    }
 };
